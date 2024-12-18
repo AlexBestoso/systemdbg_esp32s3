ee6177e853184e0984d819a1369343dd0dc5274d
Morning Star <alexbestoso@gmail.com>
2024-12-18 06:59:18 -0500

    Created getter and setter prototypes

--- src/systemReg.class.h
+++ src/systemReg.class.h
@@ -1,5 +1,9 @@
 #include "./systemReg.structs.h"
 
+#define ES3_SYSREG_BASE 0x600C0000
+#define ES3_SYSREG_READ(reg) (*(volatile uint32_t *)(DBG_PERF_SPI_BASE+reg))
+#define ES3_SYSREG_WRITE(reg, val) ((*(volatile uint32_t *)(DBG_PERF_SPI_BASE+reg)) = (val))
+
 class ES3SystemRegister{
 	private:
 
@@ -31,6 +35,66 @@ class ES3SystemRegister{
 		struct apb_ctrl_mem_power_down_reg apb_ctrl_mem_power_down;
 		struct apb_ctrl_mem_power_up_reg apb_ctrl_mem_power_up;
 
+		/*
+		 * Useful Functions
+		 * */
+		void getAll(void);
+
+		/*
+		 * Getters 
+		 * */
+		uint32_t getSystem_core_1_control_0(void);
+		uint32_t getSystem_core_1_control_1(void);
+		uint32_t getSystem_cpu_per_conf(void);
+		uint32_t getSystem_perip_clk_en0(void);
+		uint32_t getSystem_perip_clk_en1(void);
+		uint32_t getSystem_perip_rst_en0(void);
+		uint32_t getSystem_perip_rst_en1(void);
+		uint32_t getSystem_bt_lpck_div_frac(void);
+		uint32_t getSystem_cpu_intr_from_cpu_0(void);
+		uint32_t getSystem_cpu_intr_from_cpu_1(void);
+		uint32_t getSystem_cpu_intr_from_cpu_2(void);
+		uint32_t getSystem_cpu_intr_from_cpu_3(void);
+		uint32_t getSystem_rsa_pd_ctrl(void);
+		uint32_t getSystem_edma_ctrl(void);
+		uint32_t getSystem_cache_control(void);
+		uint32_t getSystem_external_device_encrypt_decrypt_control(void);
+		uint32_t getSystem_rtc_fastmem_config(void);
+		uint32_t getSystem_rtc_fastmem_crc(void);
+		uint32_t getSystem_clock_gate(void);
+		uint32_t getSystem_sysclk_conf(void);
+		uint32_t getSystem_date(void);
+		uint32_t getApb_ctrl_clkgate_force_on(void);
+		uint32_t getApb_ctrl_mem_power_down(void);
+		uint32_t getApb_ctrl_mem_power_up(void);
+
+		/*
+		 * Setters 
+		 * */
+		void setSystem_core_1_control_0(uint32_t reg);
+		void setSystem_core_1_control_1(uint32_t reg);
+		void setSystem_cpu_per_conf(uint32_t reg);
+		void setSystem_perip_clk_en0(uint32_t reg);
+		void setSystem_perip_clk_en1(uint32_t reg);
+		void setSystem_perip_rst_en0(uint32_t reg);
+		void setSystem_perip_rst_en1(uint32_t reg);
+		void setSystem_bt_lpck_div_frac(uint32_t reg);
+		void setSystem_cpu_intr_from_cpu_0(uint32_t reg);
+		void setSystem_cpu_intr_from_cpu_1(uint32_t reg);
+		void setSystem_cpu_intr_from_cpu_2(uint32_t reg);
+		void setSystem_cpu_intr_from_cpu_3(uint32_t reg);
+		void setSystem_rsa_pd_ctrl(uint32_t reg);
+		void setSystem_edma_ctrl(uint32_t reg);
+		void setSystem_cache_control(uint32_t reg);
+		void setSystem_external_device_encrypt_decrypt_control(uint32_t reg);
+		void setSystem_rtc_fastmem_config(uint32_t reg);
+		void setSystem_rtc_fastmem_crc(uint32_t reg);
+		void setSystem_clock_gate(uint32_t reg);
+		void setSystem_sysclk_conf(uint32_t reg);
+		void setSystem_date(uint32_t reg);
+		void setApb_ctrl_clkgate_force_on(uint32_t reg);
+		void setApb_ctrl_mem_power_down(uint32_t reg);
+		void setApb_ctrl_mem_power_up(uint32_t reg);
 
 	
 };
